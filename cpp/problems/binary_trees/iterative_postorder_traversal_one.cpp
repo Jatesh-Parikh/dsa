537b4526e9bf3b15ffc8ce6486ad59fd9d7d77f2
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-09 17:17:15 +0530

    add cpp/problems/trees/iterative_postorder_traversal_one

---
+++ cpp/problems/binary_trees/iterative_postorder_traversal_one.cpp
@@ -0,0 +1,46 @@
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+
+// Postorder - Left, Right, Root 
+
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+};
+
+class Solution {
+    public:
+        vector<int> iterative_postorder_traversal_one(TreeNode *root) {
+            vector<int> postorder;
+            if (root == nullptr) return postorder;
+
+            stack<TreeNode *> st1, st2;
+            st1.push(root);
+            
+            while (!st1.empty()) {
+                root = st1.top();
+                st1.pop();
+                st2.push(root);
+
+                if (root->left != nullptr) st1.push(root->left);
+                if (root->right != nullptr) st1.push(root->right);
+            }
+
+            while (!st2.empty()) {
+                postorder.push_back(st2.top()->val);
+                st2.pop();
+            }
+
+            return postorder;
+        }
+};
