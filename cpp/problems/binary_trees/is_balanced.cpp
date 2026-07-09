cd8076ec1ada2e8a3d7e4205a63402f0f72dd8c2
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-09 20:59:08 +0530

    add cpp/problems/trees/is_balanced

---
+++ cpp/problems/binary_trees/is_balanced.cpp
@@ -0,0 +1,38 @@
+// Time Complexity - O(n)
+// Space Complexity - O(n)
+
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+};
+
+class Solution {
+    private:
+        int max_depth(TreeNode *root) {
+            if (root == nullptr) return 0;
+
+            int left_height = max_depth(root->left);
+            if (left_height == -1) return -1;
+
+            int right_height = max_depth(root->right);
+            if (right_height == -1) return -1;
+
+            if(abs(left_height - right_height) > 1) return -1;
+
+            return 1 + max(left_height, right_height);
+        }
+
+    public:
+        bool is_balanced(TreeNode *root) {
+            return max_depth(root) != -1;
+        }
+};
