bddb5b0dcbf0ca732831d46fb4c436fe353ef185
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-10 23:58:08 +0530

    add cpp/problems/dp/frog_jump

---
+++ cpp/problems/dynamic_programming/frog_jump.cpp
@@ -0,0 +1,86 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+    private:
+        // Time Complexity - O(2 ^ n)
+        // Space Complexity - O(n)
+        int frog_jump_recursive(int index, vector<int>& heights) {
+            if (index == 0) return 0;
+
+            int one_step = frog_jump_recursive(index - 1, heights) + abs(heights[index] - heights[index - 1]);
+
+            int two_step = INT_MAX;
+            if(index > 1) {
+                two_step = frog_jump_recursive(index - 2, heights) + abs(heights[index] - heights[index - 2]);
+            }
+
+            return min(one_step, two_step);
+        }
+
+        // Time Complexity - O(n)
+        // Space Complexity - O(n), For both the dp vector and the recursion stack space
+        int frog_jump_memoized(int index, vector<int>& heights, vector<int>& dp) {
+            if (index == 0) return 0;
+
+            if (dp[index] != -1) return dp[index];
+
+            int one_step = frog_jump_memoized(index - 1, heights, dp) + abs(heights[index] - heights[index - 1]);
+
+            int two_step = INT_MAX;
+            if (index > 1) {
+                two_step = frog_jump_memoized(index - 2, heights, dp) + abs(heights[index] - heights[index - 2]);
+            }
+
+            dp[index] = min(one_step, two_step);
+            return dp[index];
+        }
+
+        // Time Complexity - O(n)
+        // Space Complexity - O(n)
+        int frog_jump_iterative(int index, vector<int>& heights, vector<int>& dp) {
+            dp[0] = 0;
+
+            for (int i = 1; i <= index; i++) {
+                int one_step = dp[i - 1] + abs(heights[i] - heights[i - 1]);
+
+                int two_step = INT_MAX;
+                if(i > 1) two_step = dp[i - 2] + abs(heights[i] - heights[i - 2]);
+
+                dp[i] = min(one_step, two_step);
+            }
+
+            return dp[index];
+        }
+
+        // Time Complexity - O(n)
+        // Space Complexity - O(1)
+        int frog_jump_optimized(int index, vector<int>& heights) {
+            int prev = 0, prev_2 = 0;
+
+            for (int i = 1; i <= index; i++) {
+                int one_step = prev + abs(heights[i] - heights[i - 1]);
+
+                int two_step = INT_MAX;
+                if (i > 1) two_step = prev_2 + abs(heights[i] - heights[i - 2]);
+
+                int curr = min(one_step, two_step);
+                prev_2 = prev;
+                prev = curr;
+            }
+
+            return prev;
+        }
+    
+    public:
+        void frog_jump(int n, vector<int>& heights) {
+            cout << frog_jump_recursive(n - 1, heights) << endl;
+
+            cout << frog_jump_optimized(n - 1, heights) << endl;
+        }
+};
+
+
