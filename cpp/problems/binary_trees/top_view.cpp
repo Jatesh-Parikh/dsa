e1391b0be8aa96dc5235f8ab70ead800d4553bb6
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-12 16:42:08 +0530

    add cpp/problems/trees/top_view

---
+++ cpp/problems/binary_trees/top_view.cpp
@@ -0,0 +1,56 @@
+// Time Complexity - O(n * logn)
+// Space Complexity - O(n)
+
+#include <vector>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+};
+
+class Solution {
+    public:
+        vector<int> top_view(TreeNode *root) {
+            vector<int> ans;
+
+            if(root == nullptr) return ans;
+
+            queue<pair<TreeNode *, int>> q;
+            q.push({ root, 0 });
+
+            map<int, int> mpp;
+
+            while (!q.empty()) {
+                auto it = q.front();
+                q.pop();
+
+                TreeNode *node = it.first;
+                int line = it.second;
+
+                if (mpp.find(line) == mpp.end()) mpp[line] = node->val;
+
+                if (node->left) {
+                    q.push({ node->left, line - 1 });
+                }
+
+                if (node->right) {
+                    q.push({ node->right, line + 1 });
+                }
+            }
+
+            for (auto it: mpp) {
+                ans.push_back(it.second);
+            }
+
+            return ans;
+        }
+};
