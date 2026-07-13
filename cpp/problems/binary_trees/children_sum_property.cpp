a5ed316c1f2f4524540f111288da7a458d7889fb
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-13 09:33:32 +0530

    add cpp/problems/trees/children_sum_property

---
+++ cpp/problems/binary_trees/children_sum_property.cpp
@@ -0,0 +1,37 @@
+// Time Complexity - O(n)
+// Space Complexity - O(h)
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+};
+
+class Solution {
+    public:
+        void children_sum_property(TreeNode *root) {
+            if (root == nullptr) return;
+            int child = 0;
+
+            if(root->left) child += root->left->val;
+            if(root->right) child += root->right->val;
+
+            if(child >= root->val) root->val = child;
+            else {
+                if(root->left) root->left->val = root->val;
+                if(root->right) root->right->val = root->val;
+            }
+
+            children_sum_property(root->left);
+            children_sum_property(root->right);
+
+            int total = 0;
+            if(root->left) total += root->left->val;
+            if(root->right) total += root->right->val;
+            if(root->left || root->right) root->val = total;
+        }
+};
