07c240d830663a8b65dad2aae61ec62d421f9c40
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-20 14:50:48 +0530

    add cpp/problems/arrays/count_subarray_with_sum

---
+++ cpp/problems/arrays/count_subarray_with_sum.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+    public:
+        // Time Complexity - O(n)
+        // Space Complexity - O(n)
+        int count_subarray_with_sum(vector<int>& arr, int k) {
+            unordered_map<int, int> mpp;          
+            mpp[0] = 1;
+            
+            int prefix_sum = 0, count = 0;
+
+            for (int i = 0; i < arr.size(); i++) {
+                prefix_sum += arr[i];
+                int remove = prefix_sum - k;
+                count += mpp[remove];
+                mpp[prefix_sum] += 1;
+            }
+
+            return count;
+        }
+};
