9ad1d299f8b1f6aefd37b3c30973550fb1c394cb
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-22 10:53:33 +0530

    add cpp/problems/tp_sw/max_consecutive_ones

---
+++ cpp/problems/two_pointers_and_sliding_window/max_consecutive_ones.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        // Time Complexity - O(n)
+        // Space Complexity - O(1)
+        int max_consecutive_ones(vector<int>& arr, int k) {
+            int left = 0, right = 0, max_len = 0, zeros = 0;
+            int n = arr.size();
+
+            while (right < n) {
+                if (arr[right] == 0) zeros++;
+
+                while (zeros > k) {
+                    if (arr[left] == 0) zeros--;
+                    left++;
+                }
+                
+                int len = right - left + 1;
+                max_len = max(max_len, len);
+
+                right++;
+            }
+
+            return max_len;
+        }
+
+        // Time Complexity - O(n)
+        // Space Complexity - O(1)
+        int max_consecutive_ones(vector<int>& arr, int k) {
+            int left = 0, right = 0, max_len = 0, zeros = 0;
+            int n = arr.size();
+
+            while (right < n) {
+                if (arr[right] == 0) zeros++;
+
+                if (zeros > k) {
+                    if (arr[left] == 0) zeros--;
+                    left++;
+                }
+
+                if (zeros <= k) max_len = max(max_len, right - left + 1);
+                right++; 
+            }
+
+            return max_len;
+        }
+
+};
