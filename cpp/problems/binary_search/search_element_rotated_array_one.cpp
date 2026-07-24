f6a02fee97d3d14083099afa85c7fd7fd6f79aaa
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-24 23:06:37 +0530

    add cpp/problems/bs/search_one

---
+++ cpp/problems/binary_search/search_element_rotated_array_one.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        // Time Complexity - O(logn)
+        // Space Complexity - O(1)
+        int search(vector<int>& arr, int k) {
+            int n = arr.size();
+            int low = 0, high = n - 1;
+
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                
+                if (arr[mid] == k) return mid;
+                
+                // left-half sorted
+                if (arr[low] <= arr[mid]) {
+                    if (arr[low] <= k && k <= arr[mid]) {
+                        high = mid - 1;
+                    } else { 
+                        low = mid + 1; 
+                    }
+                } else { // right-half sorted
+                    if (arr[mid]<= k  && k <= arr[high]) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+};
