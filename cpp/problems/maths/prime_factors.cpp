7594fb1ccc2fc3fc4a229c6176e3bc612f12e046
Jatesh Parikh <jateshparikh97@gmail.com>
2026-07-27 22:32:05 +0530

    add cpp/problems/maths/prime_factors

---
+++ cpp/problems/maths/prime_factors.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+// Time Complexity - O(sqrt(n))
+// Space Complexity - O(1)
+void prime_factors(int n) {
+    while (n % 2 == 0) {
+        cout << 2 << " ";
+        n /= 2;
+    }
+
+    for (int i = 3; i * i <= n; i += 2) {
+        while (n % i == 0) {
+            cout << i << " ";
+            n /= i;
+        }
+    }
+
+    if (n > 1) cout << n;
+
+    cout << endl;
+}
+
+int main () {   
+    int n;
+
+    cin >> n;
+
+    prime_factors(n);
+
+    return 0;
+}
