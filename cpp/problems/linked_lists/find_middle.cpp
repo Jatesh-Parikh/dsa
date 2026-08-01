c6368742bcf9e6545a1592fa2f08c5d957b6383d
Jatesh Parikh <jateshparikh97@gmail.com>
2026-08-01 23:08:18 +0530

    add cpp/problems/ll/find_middle

---
+++ cpp/problems/linked_lists/find_middle.cpp
@@ -0,0 +1,25 @@
+struct Node {
+    int data;
+    Node *next;
+
+    Node(): data(0), next(nullptr) {}
+    Node(int data): data(data), next(nullptr) {}
+    Node(int data, Node *next): data(data), next(next) {}
+};
+
+class Solution {
+    public:
+        // Time Complexity - O(n) 
+        // Space Complexity - O(1) 
+        Node *find_middle(Node *head) {
+            Node *slow = head;
+            Node *fast = head;
+
+            while (fast != nullptr && fast->next != nullptr) {
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+
+            return slow;
+        }
+};
