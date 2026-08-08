ff9214020dbc16d2362204a32cc57d34590a9744
Jatesh Parikh <jateshparikh97@gmail.com>
2026-08-08 16:49:07 +0530

    add cpp/problems/stacks/postfix_to_infix

---
+++ cpp/problems/stacks_and_queues/postfix_to_infix.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+    public:
+        // Time Complexity - O(n)
+        // Space Complexity - O(n)
+        string postfix_to_infix(string s) {
+            stack<string> st;
+
+            for (int i = 0; i < s.size(); i++) {
+                if (isalnum(s[i])) st.push(string(1, s[i]));
+                else {
+                    string t1 = st.top(); st.pop();
+                    string t2 = st.top(); st.pop();
+                    string x = '(' + t2 + s[i] + t1 + ')';
+                    st.push(x);
+                } 
+            }
+
+            return st.top();
+        }
+};
